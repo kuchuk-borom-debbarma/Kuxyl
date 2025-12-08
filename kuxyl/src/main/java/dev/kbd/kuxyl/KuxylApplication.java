d22ed4c8973e98c39f4f67ce790ca8b5af784ab6
kuchuk-borom-debbarma <kuchukboromd@gmail.com>
2025-12-08 23:54:04 +0530

    feat: Defined initial project structure

---
+++ kuxyl/src/main/java/dev/kbd/kuxyl/KuxylApplication.java
@@ -0,0 +1,13 @@
+package dev.kbd.kuxyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuxylApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuxylApplication.class, args);
+	}
+
+}
